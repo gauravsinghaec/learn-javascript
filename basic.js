dd0a828af375aaa86cbad5be5f09bb7b861e3da2
Gaurav Singh <gauravsingh@Gauravs-Laptop.lan>
2025-01-28 08:51:46 +0530

    learn js basic

---
+++ basic.js
@@ -0,0 +1,13 @@
+var x;
+const y = 10;
+let z = 20;
+const test = true;
+
+console.log(typeof x); // undefined
+console.log(typeof y); // undefined
+console.log(typeof z); // undefined
+
+z = "Hello World";
+console.log(typeof z); // string
+
+console.log(typeof test); // boolean
