c930da82bf72fd184f839ba2d06bb7e3c8cb013e
Gaurav Singh <gauravsingh@Gauravs-Laptop.local>
2025-01-29 08:12:16 +0530

    Add more examples of primitive type

--- basic.js
+++ basic.js
@@ -4,10 +4,18 @@ let z = 20;
 const test = true;
 
 console.log(typeof x); // undefined
-console.log(typeof y); // undefined
-console.log(typeof z); // undefined
+console.log(typeof y); // number
+console.log(typeof z); // number
 
 z = "Hello World";
 console.log(typeof z); // string
 
 console.log(typeof test); // boolean
+
+console.log("typeof null->", typeof null); // Object
+
+// const a; // Error -> Missing initializer in const declaration
+// console.log(typeof a);
+let b;
+console.log(typeof b); 
+
