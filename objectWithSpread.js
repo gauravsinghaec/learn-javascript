5d8350999534a0438cbf19a6f30bddba350daffc
Gaurav Singh <gauravsingh@Gauravs-Laptop.lan>
2025-02-04 09:01:24 +0530

    object with spread syntax

---
+++ objectWithSpread.js
@@ -0,0 +1,19 @@
+const species = { type: "Mammal", movement: "Walks" };
+
+const dog = { ...species, breed: "Labrador", color: "Golden" };
+const cat = { ...species, breed: "Siamese", color: "White" };
+
+const dog1 = {
+  type: "Mammal",
+  movement: "Walks",
+  breed: "Labrador",
+  color: "Golden",
+};
+const cat1 = {
+  type: "Mammal",
+  movement: "Walks",
+  breed: "Siamese",
+  color: "White",
+};
+
+// TODO Practice more
